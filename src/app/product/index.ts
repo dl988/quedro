0b13ddb808b47316ae79d029898b2866113dc52b
Linh Nguyen <linh.nguyen2@niteco.se>
2017-07-05 14:48:29 +0700

    Product component implementation

---
+++ src/app/product/index.ts
@@ -0,0 +1,17 @@
+import { ProductComponent } from './index/';
+import { AddProductComponent } from './add/';
+import { ProductActions } from './product.actions';
+import { ProductService } from './product.service';
+import { ProductApi } from './product.api';
+import { ProductEffect } from './product.effect';
+import { ProductReducer } from './product.reducer';
+
+export {
+  ProductComponent,
+  AddProductComponent,
+  ProductActions,
+  ProductService,
+  ProductApi,
+  ProductEffect,
+  ProductReducer
+};
---
+++ src/app/product/index/product.component.ts
@@ -0,0 +1,86 @@
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
+import { Store } from '@ngrx/store';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from "rxjs/Observable";
+import Rx from 'rxjs/Rx';
+import { UserService } from '../../user/';
+
+// Product
+import { ProductService } from '../product.service';
+import { ProductApi } from '../product.api';
+import { ProductActions } from '../product.actions';
+
+@Component({
+  selector: 'product',
+  styleUrls: [ './product.component.scss' ],
+  templateUrl: './product.component.html',
+  animations: [
+    trigger('animate', [
+      state('in', style({
+        opacity: 0
+      })),
+      state('out', style({
+        opacity: 1
+      })),
+      transition('in => out', animate('400ms')),
+      transition('out => in', animate('400ms'))
+    ]),
+  ]
+})
+export class ProductComponent implements OnInit {
+  public products: Observable<any>;
+  public user: Observable<any>;
+  static apiFetched = false;
+  public loadingStatus = !ProductComponent.apiFetched;
+  public animate = ProductComponent.apiFetched ? 'out' : 'in';
+
+  public localState = { value: '' };
+
+  constructor(
+    private store: Store<any>,
+    private route: ActivatedRoute,
+    public userService: UserService,
+    public productService: ProductService,
+    public productApi: ProductApi,
+    public productActions: ProductActions
+  ) {
+    this.products = store.select('products');
+    this.user = store.select('user');
+  }
+
+  public ngOnInit() {
+    if (!ProductComponent.apiFetched) this.fetchProducts();
+  }
+
+  public fetchProducts () {
+    let that = this;
+
+    var soldProducts = this.productApi.fetchSoldProducts();
+    var availableProducts = this.productApi.fetchAvailableProducts();
+
+    Rx.Observable.concat(soldProducts, availableProducts).reduce((acc:Array<any>, x:any) => {
+        acc.push(x); return acc;
+    }, []).subscribe(result=> {
+      var firstEmitted = result[0].json()['objects'];
+      var secondEmitted = result[1].json()['objects'];
+
+      ProductComponent.apiFetched = true;
+      that.loadingStatus = !ProductComponent.apiFetched;
+      that.animate = 'out';
+
+      that.store.dispatch(that.productActions.fetchProductsFulfilled({
+        products: [...firstEmitted, ...secondEmitted]
+      }))
+    });
+  }
+}
