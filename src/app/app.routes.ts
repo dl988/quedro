18eb589d6ec47bb329339ae4d05c5b0405521a1b
Linh Nguyen <linh.nguyen2@niteco.se>
2017-07-05 14:37:32 +0700

    Router configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,23 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home';
-import { AboutComponent } from './about';
+import { 
+  ProductComponent, 
+  AddProductComponent 
+} from './product';
+import { LoginComponent } from './login';
+import { LogoutComponent } from './logout';
 import { NoContentComponent } from './no-content';
-
-import { DataResolver } from './app.resolver';
+import { AuthGuard } from './auth-guard.service';
 
 export const ROUTES: Routes = [
-  { path: '',      component: HomeComponent },
-  { path: 'home',  component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'detail', loadChildren: './+detail#DetailModule'},
-  { path: 'barrel', loadChildren: './+barrel#BarrelModule'},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'product',  component: ProductComponent, canActivate: [AuthGuard], children: [
+    {path: 'add', component: AddProductComponent, outlet: 'sidebar'}
+  ]},
+  { path: 'login', component: LoginComponent },
+  { path: 'logout', component: LogoutComponent },
+  // { path: 'detail', loadChildren: './+detail#DetailModule'},
+  // { path: 'barrel', loadChildren: './+barrel#BarrelModule'},
   { path: '**',    component: NoContentComponent },
 ];
